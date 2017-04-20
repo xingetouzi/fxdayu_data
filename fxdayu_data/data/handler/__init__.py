d697c661e65a79ee9725794e3c13664757b59cd7
caimeng <862786917@qq.com>
2017-04-20 16:32:56 +0800

    handlers

---
+++ fxdayu_data/data/handler/__init__.py
@@ -0,0 +1,5 @@
+from fxdayu_data.data.handler.mongo_handler import MongoHandler
+from fxdayu_data.data.handler.redis_handler import RedisHandler
+
+
+__all__ = ['MongoHandler', 'RedisHandler']
---
+++ fxdayu_data/data/handler/base.py
@@ -0,0 +1,27 @@
+# encoding:utf-8
+from datetime import datetime
+from pymongo.mongo_client import database
+import pandas as pd
+import pymongo
+
+
+class DataHandler(object):
+
+    def write(self, *args, **kwargs):
+        pass
+
+    def read(self, *args, **kwargs):
+        pass
+
+    def inplace(self, *args, **kwargs):
+        pass
+
+    def update(self, *args, **kwargs):
+        pass
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    def table_names(self, *args, **kwargs):
+        pass
+
