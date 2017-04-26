40c132ea52e2b0c06c5e524499fa272006ee9ccd
BurdenBear <541795600@qq.com>
2017-04-26 18:35:10 +0800

    add dockerfile

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t registry.fxdayu.com/fxdayu_data:$1 -f dockerfile/Dockerfile .
---
+++ build_base.sh
@@ -0,0 +1,1 @@
+docker build -t registry.fxdayu.com/fxdayu_data_base:$1 -f dockerfile/Dockerfile.base .
