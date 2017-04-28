7cab29f2413a7357b9c8fdec156c0f444d3a0050
BurdenBear <541795600@qq.com>
2017-04-28 10:10:21 +0800

    merge

--- fxdayu_data/service/activate_sina.py
+++ fxdayu_data/service/activate_sina.py
@@ -2,4 +2,5 @@ from fxdayu_data.service.sina import Monitor
 
 if __name__ == '__main__':
     monitor = Monitor("sina_stock.json", db="redis_config.json", log='logging.conf')
+
     monitor.start()
