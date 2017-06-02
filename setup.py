b06f24c467150b11a7b093d391e9fd40944dcd56
caimeng <862786917@qq.com>
2017-06-02 17:37:58 +0800

    add requires

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name="fxdayu_data",
     version="0.0.1",
-    packages=["fxdayu_data.data.handler", "fxdayu_data.data.collector"]
+    packages=["fxdayu_data",
+              "fxdayu_data.data",
+              "fxdayu_data.data.handler",
+              "fxdayu_data.data.collector"],
+    requires=["pymongo>=3.4.0", "redis>=2.10.5", "tushare>=0.6.8", "requests>=2.12.4"]
 )
