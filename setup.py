c8fbee17ac6a3153ef2933812cf48aeb4dc35a8d
caimeng <862786917@qq.com>
2017-06-12 14:47:23 +0800

    install_requires packages

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
 
+
+REQUIRES = ['numpy==1.12.0',
+            'pandas==0.18.1',
+            'requests==2.12.4',
+            'redis==2.10.5',
+            'pymongo>=3.4.0',
+            'lxml==3.7.3',
+            'tushare==0.7.5',
+            'pandas-datareader==0.2.1',
+            'backports.functools_lru_cache>=1.4']
+
+
 setup(
     name="fxdayu_data",
     version="0.1.11",
@@ -8,5 +20,5 @@ setup(
               "fxdayu_data.data.handler",
               "fxdayu_data.data.collector",
               "fxdayu_data.data.data_api"],
-    requires=["pymongo", "redis", "tushare", "requests", "backports.functools_lru_cache"]
+    install_requires=REQUIRES
 )
